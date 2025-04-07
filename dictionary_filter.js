0356a8a478fa4e425d24098ab76ea6d6849e9f06
Rody ten Hoopen <rodytenhoopen@protonmail.com>
2025-04-07 17:16:23 +0200

    Prettified code

--- dictionary_filter.js
+++ dictionary_filter.js
@@ -1,11 +1,13 @@
-const fs = require('node:fs');
-const path = require('node:path'); // Add the path module
-const axios = require('axios');
+const fs = require("node:fs");
+const path = require("node:path"); // Add the path module
+const axios = require("axios");
 
 var queue = [];
-var ignore = ['brands.txt', 'cities.txt', 'countries.txt', 'names.txt'];
+var ignore = ["brands.txt", "cities.txt", "countries.txt", "names.txt"];
 var timeout = false;
-let dictionary_local = fs.readFileSync('dictionary.txt', 'UTF-8').split(/\r?\n|\r/);
+let dictionary_local = fs
+  .readFileSync("dictionary.txt", "UTF-8")
+  .split(/\r?\n|\r/);
 
 function walkDirectory(dir) {
   let contents = fs.readdirSync(dir);
@@ -14,7 +16,7 @@ function walkDirectory(dir) {
     if (fs.lstatSync(fullPath).isDirectory()) {
       walkDirectory(fullPath); // Recursively walk the directory
     } else if (fs.lstatSync(fullPath).isFile()) {
-      if (fullPath.endsWith('.txt') && !fullPath.endsWith('LICENSE')) {
+      if (fullPath.endsWith(".txt") && !fullPath.endsWith("LICENSE")) {
         let ignored = false;
         for (let k = 0; k < ignore.length; k++) {
           if (fullPath.endsWith(ignore[k])) ignored = true;
@@ -30,34 +32,34 @@ function walkDirectory(dir) {
 }
 
 async function dictionaryCheckOnline(word, attempt = 1) {
-    let options = {
-      url: `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`,
-      method: 'GET',
-      headers: { 'User-Agent': 'Mozilla/5.0' },
-    };
-  
-    try {
-      timeout = false;
-      const response = await axios(options);
-      await new Promise((resolve) => setTimeout(resolve, 25));
-      return response.data;
-    } catch (error) {
-      if (error.response && error.response.status === 429) {
-        if (timeout == false) {
-          console.log('Waiting for the API timeout to be over...');
-        }
-        timeout = true;
-        const delay = Math.pow(2, attempt) * 100;
-        await new Promise((resolve) => setTimeout(resolve, delay));
-        return dictionaryCheckOnline(word, attempt + 1); // Retry after delay
-      } else if (error.response && error.response.status == 404) {
-        timeout = false;
-        return false;
-      } else {
-        throw error; // Throw other errors
+  let options = {
+    url: `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`,
+    method: "GET",
+    headers: { "User-Agent": "Mozilla/5.0" },
+  };
+
+  try {
+    timeout = false;
+    const response = await axios(options);
+    await new Promise((resolve) => setTimeout(resolve, 25));
+    return response.data;
+  } catch (error) {
+    if (error.response && error.response.status === 429) {
+      if (timeout == false) {
+        console.log("Waiting for the API timeout to be over...");
       }
+      timeout = true;
+      const delay = Math.pow(2, attempt) * 100;
+      await new Promise((resolve) => setTimeout(resolve, delay));
+      return dictionaryCheckOnline(word, attempt + 1); // Retry after delay
+    } else if (error.response && error.response.status == 404) {
+      timeout = false;
+      return false;
+    } else {
+      throw error; // Throw other errors
     }
   }
+}
 
 function dictionaryCheck(word) {
   if (dictionary_local.indexOf(word) > -1) return true;
@@ -69,7 +71,7 @@ async function execute(dir) {
   for (let i = 0; i < queue.length; i++) {
     let path = queue[i];
     console.log(`Checking file: ${path}`);
-    let contents = await fs.readFileSync(path, 'UTF-8');
+    let contents = await fs.readFileSync(path, "UTF-8");
     let split = contents.split(/\r?\n|\r/);
     let toCheckOnline = [];
     let result = [];
@@ -86,15 +88,19 @@ async function execute(dir) {
 
     for (let k = 0; k < toCheckOnline.length; k++) {
       let selected_username = toCheckOnline[k];
-      console.log(`Checking ${selected_username} online [${k + 1}/${toCheckOnline.length}]`);
-      if (await dictionaryCheckOnline(selected_username) == true) {
+      console.log(
+        `Checking ${selected_username} online [${k + 1}/${
+          toCheckOnline.length
+        }]`
+      );
+      if ((await dictionaryCheckOnline(selected_username)) == true) {
         result.push(selected_username);
       }
     }
 
     fs.unlinkSync(path);
-    fs.appendFile(path, result.join('\n'), (err) => {});
+    fs.appendFile(path, result.join("\n"), (err) => {});
   }
 }
 
-execute(__dirname + '/wordlists');
+execute(__dirname + "/wordlists");
